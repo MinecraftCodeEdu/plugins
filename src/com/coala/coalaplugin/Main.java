ff6918de201b2975341445bfd6fcb4d0f14d80fc
leehyeongwoo <leehyeongwoo@gmail.com>
2018-12-31 15:51:21 +0900

    Add license check

--- src/com/coala/coalaplugin/Main.java
+++ src/com/coala/coalaplugin/Main.java
@@ -1,5 +1,16 @@
 package com.coala.coalaplugin;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.security.Key;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Difficulty;
 import org.bukkit.GameMode;
@@ -34,6 +45,10 @@ public class Main extends JavaPlugin {
 	private static boolean eventToggle = true;
 	public GameData gamedata;
 	
+	private static final String ALGO = "AES";
+	private static final byte[] keyValue =
+			new byte[]{'C', 'o', 'a', 'l', 'a', 'E', 'x', 'p', 'i', 'r', 'y', 'D', 'a', 't', 'e', 'e'};
+	
 	public boolean isChatPaused = false; // 채팅 제한
 	public boolean isCommandPaused = false; // 명령어 제한
 	public boolean isPlayerInvincible = false; // 플레이어 무적
@@ -46,8 +61,6 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		instance = this;
-		
-		getLogger().info("Coala Plugin Activated.");
 		
 		getCommand("move").setExecutor(new moveCommand(this));
 		getCommand("come").setExecutor(new comeCommand());
@@ -97,11 +110,12 @@ public class Main extends JavaPlugin {
 		this.isPreventExplode = true;
 		Bukkit.getLogger().info("이제 폭발로 인해 지형이 변경되지 않습니다");
 		
-//		ConfigWorld.setWorld(Bukkit.getServer().getWorlds().get(0));
-//		for(Player p : Bukkit.getOnlinePlayers()) {					
-//			p.teleport(ConfigWorld.getWorld().getSpawnLocation());
-//		}
-		//ConfigEvent.readJson(Bukkit.getWorldContainer()+"\\world\\event.json");
+    	if (validateExpireDate()) {
+    		this.getLogger().info("Coala plugin is activated.");
+    	} else {
+    		this.getLogger().warning("라이센스가 만료되었습니다. 아래의 이메일로 문의해주시기 바랍니다. webmaster@coalasw.com");
+    		Bukkit.getPluginManager().disablePlugin(this);
+    	}
 	}
 	
 	@Override
@@ -124,6 +138,87 @@ public class Main extends JavaPlugin {
 	
 	public static void setEventToggle(boolean bool) {
 		eventToggle = bool;
+	}
+	
+    /**
+     * Decrypt a string with AES algorithm.
+     *
+     * @param encryptedData is a string
+     * @return the decrypted string
+     */
+    private static String decrypt(String encryptedData) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = Base64.getDecoder().decode(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        return new String(decValue);
+    }
+    
+    /**
+     * Generate a new encryption key.
+     */
+    private static Key generateKey() throws Exception {
+        return new SecretKeySpec(keyValue, ALGO);
+    }
+    
+    /**
+     * Validate expire date.
+     */
+    private static boolean validateExpireDate() {
+		try {
+			Date date = new Date();
+			SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");	
+			
+			String encryptedData = readLicenseFile();
+			
+			if(encryptedData == null) {
+				return false;
+			} else {
+				String decryptedData = decrypt(encryptedData);
+				if(decryptedData.substring(0, 5).equalsIgnoreCase("alaoc") &&
+						decryptedData.substring(decryptedData.length()-3, decryptedData.length()).equalsIgnoreCase("wws")) {
+					int currentDate = Integer.parseInt(df.format(date));
+					int expiryDate = Integer.parseInt(decryptedData.substring(5, 13));
+					
+					if (currentDate <= expiryDate) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+				else {
+					return false;
+				}
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return false;
+    }
+    
+	/**
+	 * Read coalacraft license file.
+	 */
+	private static String readLicenseFile() {
+		try {
+	        BufferedReader br = new BufferedReader(new FileReader(Paths.get(Bukkit.getWorldContainer().toString(), "license", "LICENSE").toString()));
+	        while(true) {
+	            String line = br.readLine();
+	            if (line==null) break;
+	            br.close();
+	            return line;       
+	        }
+	        br.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
 	}
 	
 }
